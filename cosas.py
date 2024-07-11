d728823301d256175dbed825771f788a40a29bdb
Invhri <quepasamartin@gmail.com>
2024-07-11 01:37:37 -0400

    cambios

--- cosas.py
+++ cosas.py
@@ -1,13 +1,17 @@
 
 flag=True
-
+listaProductos=[]
 while flag:
     print("")
     with open('test.txt.txt', 'w', encoding='utf-8') as test:
         test.write("forting")
 
 
-
+with open('Achivo_nuevo.txt','w', encoding='utf-8') as archivo:
+        archivo.write("\tCódigo    |    nombre  |   cantidad    |   precio     \n")
+        for producto in listaProductos:
+            archivo.write(f"{producto}\n")
+        print("Se creo correctamente")
 
 
 
